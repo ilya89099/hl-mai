30a430f441334e12b44e3226f4ba06ec1f46e67e
Ilya <ilya.semenov89099@yandex.ru>
2023-12-12 13:23:05 +0300

    ...

--- order_service/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "http_order_server.h"
-
-int main(int argc, char* argv[]) {
-  HTTPOrderServer app;
-  return app.run(argc, argv);
-}
--- service_service/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "http_service_server.h"
-
-int main(int argc, char* argv[]) {
-  HTTPServiceServer app;
-  return app.run(argc, argv);
-}
